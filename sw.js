94091a3d484c43b5e08c9e809e4a6a12a6562053
Luiz-409 <1527352@sga.pucminas.br>
2026-06-25 17:57:07 -0300

    last upddate

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "ninou-v62";
+const CACHE_NAME = "ninou-v63";
 const APP_SHELL = [
   "/",
   "/index.html",
-  "/styles.css?v=62",
-  "/app.js?v=62",
+  "/styles.css?v=63",
+  "/app.js?v=63",
   "/manifest.webmanifest",
   "/icons/icon-192.png",
   "/icons/icon-512.png",
