8c4fd86bdab6a11ee16fb7500ee98cc90d9e6d68
Luiz-409 <1527352@sga.pucminas.br>
2026-07-09 20:26:34 -0300

    show

--- js/app.legacy.js
+++ js/app.legacy.js
@@ -897,7 +897,7 @@ let avatarEditorForceOpen = false;
 let avatarModalScrollRestoreY = 0;
 let avatarModalScrollLocked = false;
 
-const BABY_AVATAR_ASSET_VERSION = "75.75.87";
+const BABY_AVATAR_ASSET_VERSION = "75.75.88";
 
 function avatarAsset(path) {
   return `${path}?v=${BABY_AVATAR_ASSET_VERSION}`;
